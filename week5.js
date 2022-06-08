326b8a668220f225a0e05f535bfac86a16aa496e
UyliaRa <bachkovska@gmail.com>
2022-06-08 17:18:15 +0300

    Icons is changes depending of data!!!

--- week5.js
+++ week5.js
@@ -37,7 +37,21 @@ function showCity(event) {
   }
 }
 
+/*let icons = [
+    "src/images/cloud.png",
+    "src/images/cloudy.png",
+    "src/images/lightening.png",
+    "src/images/rain.png",
+    "src/images/rainy-day.png",
+    "src/images/snowfall.png",
+    "src/images/sun_clouds.png",
+    "src/images/sun.png",
+    "src/images/wind.png",
+  ];*/
+
 function showWeather(response) {
+  let yourCity = response.data.name;
+  let searchedCity = document.querySelector("#searchedCity");
   let temperatureNow = response.data.main.temp;
   let currentTemperature = document.querySelector("#currentTemperature");
   let humidityNow = response.data.main.humidity;
@@ -46,12 +60,90 @@ function showWeather(response) {
   let currentWind = document.querySelector("#wind");
   let skyNow = response.data.weather[0].description;
   let currentSkyCondition = document.querySelector("#skyCondition");
+  let conditionNow = response.data.weather[0].description;
+  let iconElement = document.querySelector("#iconOfNow");
+
+  searchedCity.innerHTML = yourCity;
   currentTemperature.innerHTML = Math.round(temperatureNow);
   currentHumidity.innerHTML = humidityNow;
   currentWind.innerHTML = windNow;
   currentSkyCondition.innerHTML = skyNow;
-}
 
+  if (conditionNow === "clear sky") {
+    iconElement.setAttribute("src", "src/images/sun.png");
+  } else if (conditionNow === "few clouds") {
+    iconElement.setAttribute("src", "src/images/sun_clouds.png");
+  } else if (conditionNow === "scattered clouds") {
+    iconElement.setAttribute("src", "src/images/cloudy.png");
+  } else if (conditionNow === "broken clouds" || "overcast clouds") {
+    iconElement.setAttribute("src", "src/images/cloud.png");
+  } else if (
+    conditionNow === "light rain" ||
+    "moderate rain" ||
+    "light intensity shower rain" ||
+    "light intensity drizzle" ||
+    "drizzle" ||
+    "heavy intensity drizzle" ||
+    "light intensity drizzle rain" ||
+    "drizzle rain" ||
+    "heavy intensity drizzle rain" ||
+    "shower rain and drizzle" ||
+    "heavy shower rain and drizzle" ||
+    "shower drizzle"
+  ) {
+    iconElement.setAttribute("src", "src/images/rainy-day.png");
+  } else if (
+    conditionNow === "heavy intensity rain" ||
+    "very heavy rain" ||
+    "extreme rain" ||
+    "freezing rain" ||
+    "shower rain" ||
+    "	heavy intensity shower rain" ||
+    "ragged shower rain"
+  ) {
+    iconElement.setAttribute("src", "src/images/rain.png");
+  } else if (
+    conditionNow === "thunderstorm with light rain" ||
+    "thunderstorm with rain" ||
+    "thunderstorm with heavy rain" ||
+    "	light thunderstorm" ||
+    "thunderstorm" ||
+    "heavy thunderstorm" ||
+    "ragged thunderstorm" ||
+    "thunderstorm with light drizzle" ||
+    "thunderstorm with drizzle" ||
+    "thunderstorm with heavy drizzle"
+  ) {
+    iconElement.setAttribute("src", "src/images/lightening.png");
+  } else if (
+    conditionNow === "light snow" ||
+    "Snow" ||
+    "Heavy snow" ||
+    "Sleet" ||
+    "Light shower sleet" ||
+    "Shower sleet" ||
+    "Light rain and snow" ||
+    "Rain and snow" ||
+    "Light shower snow" ||
+    "Shower snow" ||
+    "Heavy shower snow"
+  ) {
+    iconElement.setAttribute("src", "src/images/snowfall.png");
+  } else if (
+    conditionNow === "mist" ||
+    "Smoke" ||
+    "Haze" ||
+    "sand/ dust whirls" ||
+    "fog" ||
+    "sand" ||
+    "dust" ||
+    "volcanic ash" ||
+    "squalls" ||
+    "tornado"
+  ) {
+    iconElement.setAttribute("src", "src/images/wind.png");
+  }
+}
 function searchWeatherData(response) {
   let apiKey = "c4e197dbfea53d6e2014f3499d598de9";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=";
@@ -94,15 +186,6 @@ function getTempDataCels(event) {
 let goToCelsius = document.querySelector("#celsius");
 goToCelsius.addEventListener("click", getTempDataCels);
 
-function showCurrentData(response) {
-  let yourCity = response.data.name;
-  let searchedCity = document.querySelector("#searchedCity");
-  searchedCity.innerHTML = yourCity;
-  let yourTemp = response.data.main.temp;
-  let currentTemperature = document.querySelector("#currentTemperature");
-  currentTemperature.innerHTML = Math.round(yourTemp);
-}
-
 function currentPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -110,7 +193,7 @@ function currentPosition(position) {
   let apiKey = "c4e197dbfea53d6e2014f3499d598de9";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showCurrentData);
+  axios.get(apiUrl).then(showWeather);
 }
 
 function goToNavigator(event) {
